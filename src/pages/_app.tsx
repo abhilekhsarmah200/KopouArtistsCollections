e5b9664141df4254b2f0dff600181691b0ceb50c
Abhilekh Sarmah <64719469+abhilekhsarmah200@users.noreply.github.com>
2025-10-14 19:45:51 +0530

    Add render blocking

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,22 @@ import type { AppProps } from 'next/app';
 import Navbar from './components/Navbar';
 import { useState } from 'react';
 import Footer from './components/footer';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      {/* Load after page becomes interactive */}
+      <Script
+        src='https://analytics.example.com/script.js'
+        strategy='afterInteractive'
+      />
+
+      {/* Load during browser idle time */}
+      <Script
+        src='https://chat-widget.example.com/chat.js'
+        strategy='lazyOnload'
+      />
       <Navbar />
       <Component {...pageProps} />
       <Footer />

c806f5ebd891448c00ecc5eb0b01f4c2128ab943
Abhilekh Sarmah <64719469+abhilekhsarmah200@users.noreply.github.com>
2025-10-15 07:33:39 +0530

    Add twitter meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -48,6 +48,25 @@ export default function Document() {
           property='og:image'
           content='https://res.cloudinary.com/dwkajs0ca/image/upload/v1760073679/KopouArtistsCollectionsIcon/Webp/icon2.webp'
         />
+        <meta property='og:site_name' content='Kopou For Artists' />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta
+          name='twitter:title'
+          content='Kopou For Artists - Digital Art Portfolio'
+        />
+        <meta
+          name='twitter:description'
+          content='Celebrating Artists, Culture & Collaboration'
+        />
+        <meta
+          name='twitter:image'
+          content='https://res.cloudinary.com/dwkajs0ca/image/upload/v1760073679/KopouArtistsCollectionsIcon/Webp/icon2.webp'
+        />
+
+        <meta
+          name='image'
+          content='https://res.cloudinary.com/dwkajs0ca/image/upload/v1760073679/KopouArtistsCollectionsIcon/Webp/icon2.webp'
+        />
       </Head>
       <body>
         <Main />
